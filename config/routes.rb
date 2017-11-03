e154c10b855a18a2202fb9df4644cb667de2e8fd
Paul <paul.du56@gmail.com>
2017-11-03 18:34:20 -0400

    ajout recherche trajet

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :trajets
   end
 
+  resources :search
+
   root :to =>'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
