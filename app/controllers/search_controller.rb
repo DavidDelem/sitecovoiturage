b76a83c283d3ead5d8212d3fa9c50a46072d86d2
Paul <paul.du56@gmail.com>
2017-11-05 13:38:46 -0500

    choisir la date d'un rajet recherché

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def index
     if(params.has_key?(:ville_depart) && params.has_key?(:ville_destination)) then
-      @trajet = Trajet.where("ville_depart = ? AND ville_destination = ?", params[:ville_depart], params[:ville_destination])
+      @trajet = Trajet.where("ville_depart = ? AND ville_destination = ? AND horaire_depart >= ?", params[:ville_depart], params[:ville_destination], params[:horaire_depart])
     else
       @trajet = Trajet.all
     end
