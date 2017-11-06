5a6691e1724a9d61c7a1c297d6335d6327a5fe52
Paul <paul.du56@gmail.com>
2017-11-06 15:26:27 -0500

    ajout page et menu preference

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :members
   resources :members, only: [:show] do
+    get '/preferences', to: 'members#preferences'
     resources :trajets
   end
 
   resources :search
 
   root :to =>'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
