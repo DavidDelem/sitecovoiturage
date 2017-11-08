cd4049a096fdf413cff6920d778884e6d28ed905
Paul <paul.du56@gmail.com>
2017-11-08 01:40:33 -0500

    cleanup & preferences OK

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,8 +8,9 @@ class SearchController < ApplicationController
     @month_depart = params[:month]
     @day_depart = params[:day]
 
-    if(params[:ville_depart].present? && params[:ville_destination].present?) then
+    if params[:ville_depart] && params[:ville_destination] && !params[:ville_depart].empty? && !params[:ville_destination].empty? then
       if !params["year"] && !params["month"] && !params["day"] then
+
         date_depart_min = DateTime.now.beginning_of_day
         date_depart_max = DateTime.now.at_end_of_day.localtime
 
@@ -22,11 +23,17 @@ class SearchController < ApplicationController
         date_depart_max = DateTime.new(params["year"].to_i, params["month"].to_i, params["day"].to_i, 23 ,59 ,59)
       end
       @trajet = Trajet.where("ville_depart = ? AND ville_destination = ? AND horaire_depart BETWEEN ? AND ?", params[:ville_depart], params[:ville_destination], date_depart_min, date_depart_max)
+      if @trajet.empty? then
+        @error = "Pas de covoiturage trouvé"
+      end
     else
-      @trajet = Trajet.all
+      @trajet = nil;
+      @error = "Ville de départ ou ville d'arrivée non renseignée"
     end
   end
 
   private
-
+  def article_params
+    params.permit(:ville_depart, :ville_destination)
+  end
 end
