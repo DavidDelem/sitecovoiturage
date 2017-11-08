77234d734b8dd30a272b3f645761f56aa32fb55d
Paul <paul.du56@gmail.com>
2017-11-07 23:59:06 -0500

    recherche accueil ok

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,10 +9,17 @@ class SearchController < ApplicationController
     @day_depart = params[:day]
 
     if(params[:ville_depart].present? && params[:ville_destination].present?) then
-
-      date_depart = DateTime.new(params["year"].to_i, params["month"].to_i, params["day"].to_i, 0 ,0 ,0)
-      puts date_depart
-      @trajet = Trajet.where("ville_depart = ? AND ville_destination = ? AND horaire_depart >= ?", params[:ville_depart], params[:ville_destination], date_depart)
+      if !params["year"] && !params["month"] && !params["day"] then
+        date_depart_min = DateTime.now.beginning_of_day
+        date_depart_max = DateTime.now.at_end_of_day.localtime
+        @year_depart = DateTime.now.year
+        @month_depart = DateTime.now.month
+        @day_depart = DateTime.now.day
+      else
+        date_depart_min = DateTime.new(params["year"].to_i, params["month"].to_i, params["day"].to_i, 0 ,0 ,0)
+        date_depart_max = DateTime.new(params["year"].to_i, params["month"].to_i, params["day"].to_i, 23 ,59 ,59)
+      end
+      @trajet = Trajet.where("ville_depart = ? AND ville_destination = ? AND horaire_depart BETWEEN ? AND ?", params[:ville_depart], params[:ville_destination], date_depart_min, date_depart_max)
     else
       @trajet = Trajet.all
     end
