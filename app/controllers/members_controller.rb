7d59533ea7da0a3cd682d1cb2b3542b3ff17f4d0
Paul <paul.du56@gmail.com>
2017-11-12 14:31:14 -0500

    ajout changement image de profil

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -16,10 +16,6 @@ class MembersController < ApplicationController
     @moyenneNotes = totalNotes / @nbTrajet;
   end
 
-
-
-
-
   def preferences
     @member = Member.find(params[:member_id])
   end
@@ -35,10 +31,30 @@ class MembersController < ApplicationController
 
   end
 
+  def changeimage
+    @member = Member.find(params[:member_id])
+  end
+
+  def upload
+    @member = Member.find(params[:member_id])
+    uploaded_io = params[:picture]
+    newImgName = Time.now.to_i.to_s + uploaded_io.original_filename
+    File.open(Rails.root.join('app', 'assets', 'images','profil', newImgName), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    @member.update(image: newImgName)
+    redirect_to member_changeimage_url(@member)
+  end
+
+
   private
 
   def member_preferences
     params.permit(:preference_discussion, :preference_cigarette, :preference_animaux, :preference_musique)
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
   end
 end
 
